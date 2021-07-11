be878959d3b28d6bec17dc8587add7b24b7050e3
zoukaiyuan <opensource_@123.com>
2021-07-11 08:22:12 +0800

    user

---
+++ User.java
@@ -0,0 +1,9 @@
+package com.zky;
+
+public class User {
+    public static void main(String[] args) {
+        UserSingleton us1 = UserSingleton.getInstance();
+        UserSingleton us2 = UserSingleton.getInstance();
+        System.out.println(us1 == us2);
+    }
+}
